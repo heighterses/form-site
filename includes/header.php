452429af7b23f93697f7b0232e465c72ea84fab0
heighterses <binaftaba@gmail.com>
2025-06-11 00:23:47 +0500

    adding new UI in this site

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student Management System</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>">
 </head>
 <body>
     <div class="header">
